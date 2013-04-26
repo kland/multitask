9013590f97799634f9e321644709dc043776c808
Karl Landström <karl.landstrom@ki.se>
2013-04-26 12:32:08 +0200

    Small improvement in function multitask_lasso

--- src/multitask.cpp
+++ src/multitask.cpp
@@ -5,29 +5,27 @@
 SEXP multitask_lasso(SEXP X0, SEXP y0, SEXP lambda0, SEXP eps0)
 {
 	//convert parameters to Rcpp types
-	Rcpp::NumericMatrix A(X0); //using name A to be consistent with Shotgun library
+	Rcpp::NumericMatrix X(X0);
 	Rcpp::NumericVector y(y0);
 	Rcpp::NumericVector lambda(lambda0); //one-element vector
 	Rcpp::NumericVector eps(eps0); //one-element vector
 	
-	shotgun_data data;
+	shotgun_data data; //input to and output from function solveLasso
 
-	//init data.A_cols and data.A_rows
-	int M = A.nrow();
-	int N = A.ncol();
+	//initialize input fields (shotgun calls the design matrix A instead of X)
+	int M = X.nrow();
+	int N = X.ncol();
 	data.A_cols.resize(N);
 	data.A_rows.resize(M);
 	for (int k = 0; k < M * N; k++) {
-		if (A[k] != 0.0) {
+		if (X[k] != 0.0) {
 			int i = k / N; //row
 			int j = k % N; //column
-			data.A_cols.at(j).add(i, A[k]);
-			data.A_rows.at(i).add(j, A[k]);
+			data.A_cols.at(j).add(i, X[k]);
+			data.A_rows.at(i).add(j, X[k]);
 		}
 	}
-
 	data.y = Rcpp::as< std::vector<valuetype_t> >(y);
-
 	data.nx = N;
 	data.ny = M;
 
