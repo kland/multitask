4b8870f3f9e4e7a71d44d6e44c0b26e17613b045
Karl Landström <karl.landstrom@ki.se>
2013-04-26 14:53:08 +0200

    Add Alexandra's R implementation. SimStudy scripts have been turned into functions and some functions have been renamed in order to avoid name clashes.

---
+++ R/MultitaskFunctions-Linear.R
@@ -0,0 +1,144 @@
+
+
+#solving the garotte problem 
+solveGarotte.linear<-function(y,X,lambda=1,eps=1e-12){
+  require(quadprog)
+  # quadprog: solving quadratic programming problems of the
+  # form min(-d^T b + 1/2 b^T D b) with the constraints A^T b >= b_0.
+
+  n <- length(y) 	# n: number of total replicates	
+  K <- ncol(X) 	# K: number of groups
+	
+  # check for zero or low eigenvalues, extra to check if system is solvable.
+  dout<-rep(0,K)
+  pick1<-apply(abs(X),2,sum)>eps
+  X<-X[,pick1,drop=F]
+  if (ncol(X)==0){
+    return(dout)
+  }
+  pick2<-!abs(eigen(t(X) %*% X)$values)<eps
+  X<-X[,pick2,drop=F]
+	
+  # K: new number of groups
+  K <- ncol(X)
+	
+  if(is.null(dim(X)))
+    return(dout)
+	
+  linvec <- t(t(y) %*% X - rep(lambda,K))
+  quadmat <- t(X) %*% X
+	
+  A <- diag(K)
+  d0<-rep(0,K)
+	
+  try(sol<-solve.QP(quadmat, linvec, A, d0)$solution)	
+  try(sol[abs(sol)<eps]<-0)
+  try(dout[pick1][pick2]<-sol)
+
+  return(dout)
+}
+
+multitask.linear<-function(X,y,tasks,groups,lambda,eps=1e-12){
+
+  require(penalized)
+  require(glmnet)
+		
+# initial formatting
+  y<-as.numeric(y)
+  X<-data.matrix(X)
+  tasks<-as.factor(tasks)
+	
+  n <- as.numeric(table(tasks))	# replicates
+  p <- ncol(X)	                # predictors
+  K <- length(levels(tasks))    # tasks
+  L <- ncol(groups)		# groups
+
+# select random starting points  
+  dstart<-runif(L,0.5,5)
+  alphastart<-matrix(runif(p*K,-0.1,0.1),nrow=p,ncol=K)
+  etastart<-matrix(runif(L*K,0.5,5),nrow=L,ncol=K)
+  betastart<-alphastart
+		
+# variables holding new and current estimates of parameters
+  alpha.cur <- alphastart   # size: p x K 
+  d.cur <-dstart            # size: L x 1 (vector of length L)
+  eta.cur <-etastart        # size: L x K
+  beta.cur <- betastart     # size: p x K 
+
+
+# Here starts the loop that should be moved to C++
+#
+# Input arguments:
+# X,y,tasks,groups,lambda
+# alpha.cur,d.cur,eta.cur,beta.cur (i.e. starting values for the parameters, could also be assigned in C++)
+#
+# Return arguments:
+# alpha.cur,d.cur,eta.cur,beta.cur (i.e. the final values for the parameters)
+# 
+
+  converged<-FALSE
+  while(!converged){
+    # 1. update alpha
+    alpha.new<-matrix(NA,nrow=p,ncol=K)
+    for(k in 1:K){
+      task<-levels(tasks)[k]
+      # this matrix is of size n x p. Corresponds to equation I (extra notation in paper).
+      Xtilde<-X[tasks==task,] %*% diag(apply(groups %*% diag(d.cur * eta.cur[,k]),1,sum))
+      # this is the call to the Lasso solver
+      alpha.fit<-penalized(y[tasks==task],Xtilde,unpenalized = ~0,lambda1=lambda,standardize=F,trace=F)
+      alpha.new[,k]<-coef(alpha.fit,"all")
+    }
+
+#    # alternative, expanded matrix
+#    Xtilde <- list()
+#    for(k in 1:K){
+#      task<-levels(tasks)[k]
+#      Xtilde[[k]]<-(X[tasks==task,] %*% diag(apply(groups %*% diag(d.cur * eta.cur[,k]),1,sum)))
+#    }
+#    Xtilde <- as.matrix(bdiag(Xtilde))
+#    alpha.fit<-penalized(y,Xtilde,unpenalized = ~0,lambda1=lambda,standardize=F,trace=F)
+ 		
+    # 2. update d
+    # Xtilde2 is of size K*n x L after this loop. Corresponds to equation II (extra notation in paper).
+    Xtilde2 <- NULL
+    for(k in 1:K){
+      task<-levels(tasks)[k]
+       Xtilde2<-rbind(Xtilde2,((X[tasks==task,] %*% diag(alpha.new[,k])) %*% groups) %*% diag(eta.cur[,k]))
+    }
+    # this is the call to the quadprog solver
+    d.new<-solveGarotte.linear(y,Xtilde2)
+ 		
+    # 3. update eta
+    eta.new<-matrix(NA,nrow=L,ncol=K)
+    for(k in 1:K){
+      task<-levels(tasks)[k]
+      # this matrix is of size n x L. Corresponds to equation III (extra notation in paper).
+      Xtilde3<-((X[tasks==task,] %*% diag(alpha.new[,k])) %*% groups) %*% diag(d.new)
+      eta.new[,k]<-solveGarotte.linear(y[tasks==task],Xtilde3)
+    }
+ 
+    # 4. update beta
+    beta.new <- alpha.new * (groups %*% (diag(d.new) %*% eta.new))
+    
+    # check convergence
+    if (max(abs(beta.new - beta.cur))<eps){
+      converged<-TRUE
+    }
+
+    # update current estimates
+    alpha.cur<-alpha.new
+    d.cur<-d.new
+    eta.cur<-eta.new
+    beta.cur<-beta.new
+  } #end of while loop
+  
+  fit<-NULL
+  fit$beta <- beta.cur
+  fit$alpha <- alpha.cur
+  fit$d <- d.cur
+  fit$eta <- eta.cur
+  fit$tasks <- tasks
+  fit$groups <- groups
+  fit$lambda <- lambda
+  fit
+}
---
+++ R/SimStudy-Linear.R
@@ -0,0 +1,55 @@
+simstudy.linear <- function () {
+
+	# First test case with half of the genes active, and one task killed for one group.
+
+	set.seed(15678)
+	#set up parameters (small problem)
+	n <- 100	# replicates
+	p <- 21		# predictors
+	L <- 4		# groups
+	K <- 3		# tasks
+
+	#set up groups for genes (predictors)
+	groups <- matrix(0,nrow=p,ncol=L)
+	rownames(groups)<-paste('gene',1:p,sep=' ');colnames(groups)<-paste('group',1:L,sep=' ')
+	offset<-0
+	for (g in 1:L){
+	  groups[(offset+1):min(offset+6,p),g] <- 1
+	  offset <- offset + 5
+	}
+
+	#generate X (feature) matrix
+	X <- array(rnorm(n*p*K), dim=c(n,p,K))
+
+	#generate true regression coefficients
+	w <- matrix(0,nrow=p,ncol=K)
+	w[groups[,1]+groups[,2]>0,]<-runif(K*sum(groups[,1]+groups[,2]>0),min=0.2,max=2.5)*sample(c(-1,1),size=K*sum(groups[,1]+groups[,2]>0),replace=T )
+	w[1:5,3]<-0
+	w[9,1]<-0
+	colnames(w)<-paste('task ',1:K,sep=' ')
+	rownames(w)<-paste('gene',1:p,sep=' ')
+
+	#generate linear responses
+	y<-NULL
+	for(k in 1:K){
+	  y <- cbind(y, (X[,,k]%*%w[,k] + rnorm(n,0,1.5)))
+	}	
+
+	#reformat data
+	y<-as.numeric(y)
+	Xtemp<-NULL
+	for(k in 1:K){
+	  Xtemp<-rbind(Xtemp, X[,,k])
+	}
+	X<-Xtemp
+	
+	#make task indicator vector
+	tasks<-rep(LETTERS[1:K],each=n)
+
+	#standardize data
+	y<-as.numeric(unlist(tapply(y,tasks,scale,scale=F,simplify=T)))
+	X<-matrix(unlist(apply(X,2,tapply,tasks,scale)),ncol=p)	
+
+	#run method for a sequence of lambdas
+	out <- multitask.linear(X,y,tasks,groups,lambda=40)
+}
