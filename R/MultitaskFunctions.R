e9a6f825b2fb1585a163031b3f1bc8d316728f19
aleja067 <alexandra.jauhiainen@ki.se>
2013-10-22 14:26:34 +0200

    Added group lasso function

--- R/MultitaskFunctions.R
+++ R/MultitaskFunctions.R
@@ -51,6 +51,52 @@ multitask <- function(X, y, tasks, groups, lambda=NULL, nlambda=20, model="linea
   fit
 }
 
+grplasso <- function(X, y, groups, lambda=NULL, nlambda=20, model="linear", standardize=T,eps=1e-6,maxiter=500,maxiter.shotgun=300) {
+
+  n <- length(y)		        # replicates
+  p <- ncol(X)			        # predictors
+  L <- ncol(groups)			# groups
+  tasks <- as.factor(rep("A",n))        # set tasks, all the same
+ 
+  if (model == "linear") {
+    model.num <- 0
+  } else if(model=="logistic") {
+    model.num <- 1
+    y <- 1*(y==max(y))
+  }
+
+  if(standardize){
+    if(model=="linear")
+      y<-as.numeric(unlist(tapply(y,tasks,scale,scale=F,simplify=T)))
+    X<-matrix(unlist(apply(X,2,tapply,tasks,scale)),ncol=p)	
+  }
+
+   if(is.null(lambda)){
+    lambda <- calcLambda(X,y,tasks,nlambda,model)
+  }
+  lambda <- sort(lambda,decreasing=T)
+  
+  fit <- NULL; nlambda <- length(lambda)
+   for(i in 1:nlambda){
+     temp.fit <- .Call("multitask", X, y, n, groups, lambda[i], model.num, eps, maxiter, maxiter.shotgun, PACKAGE = "multitask")
+     if(temp.fit$converged){
+       fit$converged <- c(fit$converged, temp.fit$converged) 
+       fit$beta <- cbind(fit$beta, as.numeric(temp.fit$beta))
+       fit$alpha <- cbind(fit$alpha, as.numeric(temp.fit$alpha))
+       fit$eta <- cbind(fit$eta,as.numeric(temp.fit$eta))
+       fit$d <- cbind(fit$d, temp.fit$d)
+       fit$bic <- c(fit$bic, as.numeric(temp.fit$bic)) 
+       fit$lambda <- c(fit$lambda,lambda[i])
+     }
+   }
+
+  if(is.null(fit)){
+    warning("No lambdas converged. Try to give larger lambdas or, as a second option, increase the maxiter parameter.")
+  }
+  
+  fit$groups <- groups
+  fit
+}
 
 calcLambda <- function(X,y,tasks,nlambda,model){
     lams <- c()
@@ -74,4 +120,3 @@ calcLambda <- function(X,y,tasks,nlambda,model){
     lambda <- seq(lambda.max,lambda.min,length.out=nlambda)
     lambda
 }
-
